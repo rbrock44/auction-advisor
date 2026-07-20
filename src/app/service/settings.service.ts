ed9729d5068ee10c33f005717f74a6560bf1486a
rbrock44 <rbrock444@gmail.com>
2026-07-19 21:41:43 -0500

    upgrade to angular 13

--- src/app/service/settings.service.ts
+++ src/app/service/settings.service.ts
@@ -19,7 +19,7 @@ export class SettingsService implements OnDestroy {
   donationsSubject: Subject<Donation[]> = new Subject<Donation[]>();
   productsSubject: Subject<Product[]> = new Subject<Product[]>();
   purchasesSubject: Subject<Purchase[]> = new Subject<Purchase[]>();
-  // tslint:disable-next-line:variable-name
+  // eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle, id-blacklist, id-match
   private _settingsResetSubject: Subject<boolean> = new Subject<boolean>();
 
   title: string;
