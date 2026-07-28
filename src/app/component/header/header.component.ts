b6b663f1eb76946042177b44f7ff4395c0b23f56
rbrock44 <rbrock444@gmail.com>
2026-07-28 18:54:58 -0500

    redo scss app wide

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -5,41 +5,7 @@ import { SettingsService } from '../../service/settings.service';
 
 @Component({
     selector: 'app-header',
-    template: `
-    <div>
-      <div class="nav-bar-bar">
-        <nav mat-tab-nav-bar>
-          <div class="nav-bar-div">
-            <a mat-tab-link
-               mat-theme="accent"
-               (click)="show(0)"
-               class="nav-bar-link"
-               data-products-nav>Products</a>
-            <a mat-tab-link
-               mat-theme="accent"
-               (click)="show(1)"
-               class="nav-bar-link"
-               data-people-nav>People</a>
-            <a mat-tab-link
-               mat-theme="accent"
-               (click)="show(2)"
-               class="nav-bar-link"
-               data-purchases-nav>Purchases</a>
-            <a mat-tab-link
-               mat-theme="accent"
-               (click)="show(3)"
-               class="nav-bar-link"
-               data-donations-nav>Donations</a>
-            <a mat-tab-link
-               mat-theme="accent"
-               (click)="show(4)"
-               class="nav-bar-link"
-               data-settings-nav>Settings</a>
-          </div>
-        </nav>
-      </div>
-    </div>
-  `,
+    templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     changeDetection: ChangeDetectionStrategy.Eager,
     standalone: false
